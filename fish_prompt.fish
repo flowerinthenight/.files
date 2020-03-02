3edc7979ba87db74cd14422f3bd0913efa7dc4dd
flowerinthenight <chew.esmero@gmail.com>
2020-03-02 20:23:35 +0900

    fish conf

---
+++ fish_prompt.fish
@@ -0,0 +1,45 @@
+# Ref: https://medium.com/@joshuacrass/git-fish-prompt-faa389fff07c
+# Options
+set __fish_git_prompt_show_informative_status
+set __fish_git_prompt_showcolorhints
+set __fish_git_prompt_showupstream "informative"
+
+# Colors
+set green (set_color green)
+set magenta (set_color magenta)
+set normal (set_color normal)
+set red (set_color red)
+set yellow (set_color yellow)
+
+set __fish_git_prompt_color_branch magenta --bold
+set __fish_git_prompt_color_dirtystate white
+set __fish_git_prompt_color_invalidstate red
+set __fish_git_prompt_color_merging yellow
+set __fish_git_prompt_color_stagedstate yellow
+set __fish_git_prompt_color_upstream_ahead green
+set __fish_git_prompt_color_upstream_behind red
+
+# Icons
+set __fish_git_prompt_char_cleanstate '✔'
+set __fish_git_prompt_char_conflictedstate 'conflict️'
+set __fish_git_prompt_char_dirtystate 'dirty:'
+set __fish_git_prompt_char_invalidstate 'invalid'
+set __fish_git_prompt_char_stagedstate 'staged:'
+set __fish_git_prompt_char_stashstate 'stash'
+set __fish_git_prompt_char_stateseparator '|'
+set __fish_git_prompt_char_untrackedfiles 'untracked:'
+set __fish_git_prompt_char_upstream_ahead '+'
+set __fish_git_prompt_char_upstream_behind '-'
+set __fish_git_prompt_char_upstream_diverged 'diverged'
+set __fish_git_prompt_char_upstream_equal '=' 
+
+function fish_prompt
+  set last_status $status
+
+  set_color $fish_color_cwd
+  printf '%s@%s [%s]' (whoami) (hostname) (prompt_pwd)
+  set_color normal
+
+  printf '%s ' (__fish_git_prompt)
+  set_color normal
+end
