3972955d8dc92d5e7f0ac73eb131954cce04342c
flowerinthenight <flowerinthenight@pm.me>
2022-11-01 05:24:25 +0000

    chore: fmt

--- goset.sh
+++ goset.sh
@@ -2,9 +2,9 @@
 
 $1 version &>/dev/null
 if [ $? -eq 0 ]; then
-    ln -sf $GOPATH/bin/$1 $HOME/.local/bin/go
-    go version
-    exit 0
+  ln -sf $GOPATH/bin/$1 $HOME/.local/bin/go
+  go version
+  exit 0
 fi
 
 go install golang.org/dl/$1@latest && $1 download
